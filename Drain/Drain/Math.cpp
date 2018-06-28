829ca837099d6d3924725c647acee19ec1bbc284
maxrchung <maxrchung@gmail.com>
2018-06-28 01:26:59 -0700

    Added RandomOffset helper class to help calculate a random offset; added RadianToDegree and DegreeToRadian helper functions for conversions

--- Drain/Drain/Math.cpp
+++ Drain/Drain/Math.cpp
@@ -1,3 +1,11 @@
 #include <math.h>
 #include "Math.hpp"
 const float Math::sqrt2 = sqrtf(2);
+float Math::degreesToRadians(const float degrees) {
+	const auto radians = degrees * 180.0f / pi;
+		return radians;
+}
+float Math::radiansToDegrees(const float radians) {
+	const auto degrees = radians * pi / 180.0f;
+	return degrees;
+}
