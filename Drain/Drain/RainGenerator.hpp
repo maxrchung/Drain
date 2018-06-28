4ded40ce7eeb6c0e747d09af8fee4b8a08845143
Alex Liu <34083577+Naranja-Sagged@users.noreply.github.com>
2018-06-28 03:02:01 -0700

    Split RainGenerator up into functions

--- Drain/Drain/RainGenerator.hpp
+++ Drain/Drain/RainGenerator.hpp
@@ -6,9 +6,19 @@
 class RainGenerator {
 public:
 	RainGenerator(int rainCount, int dropCount, Time startTime, Time endTime, float acceleration = 1.0f);
-	float VelocityController(float dropTotalTime, float acceleration);
+	void VelocityController();
 	std::vector<Sprite*> sprites;
 
 private:
+	const int rainCount;
+	const int dropCount;
+	const Time startTime;
+	const Time endTime;
+	const float acceleration;
+
 	float minDropTime = 10.0f;
+	const float leftOfScreen;
+	float dropTotalTime;
+	int totalTime;
+	float rainSpacing;
 };
