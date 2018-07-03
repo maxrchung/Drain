d298ea490cbee226f4d73579864c06d3f88e0593
maxrchung <maxrchung@gmail.com>
2018-07-02 20:28:07 -0700

    Recommitting work I accidentally reverted

--- Drain/Drain/RainGenerator.cpp
+++ Drain/Drain/RainGenerator.cpp
@@ -3,37 +3,43 @@
 #include "Path.hpp"
 #include <cmath>
 
-RainGenerator::RainGenerator(int rainCount, int dropCount, Time startTime, Time endTime, float acceleration) {
+RainGenerator::RainGenerator(int rainCount, int dropCount, Time startTime, Time endTime, float acceleration)
+	: rainCount{ rainCount }, dropCount{ dropCount }, startTime{ startTime }, endTime{ endTime }, acceleration{ acceleration },
+	leftOfScreen{ -Vector2::ScreenSize.x / 2 }, totalTime{ endTime.ms - startTime.ms }, rainSpacing{ Vector2::ScreenSize.x / (rainCount - 1) } {
 	//Parameter Big think: count, time start, time end, density(of a row of rain) angle, speed, acceleration, dropcount?
 	//acceleration: raindrop accelerates after the current one finishes dropping
 	//Note: acceleration only supports 0-2
 
-	int leftOfScreen = -Vector2::ScreenSize.x / 2;
-	int rainSpacing = Vector2::ScreenSize.x / (rainCount-1);
-	int totalTime = endTime.ms - startTime.ms;
-	int dropTotalTime = totalTime / dropCount;
+	//Todo: randomize sprite size, rain position, rain tilt
 
-	int dropStartTime = startTime.ms;
-	int dropEndTime = dropStartTime + dropTotalTime;
+
+	//Initiate drop time values
+	dropTotalTime = totalTime / dropCount;
+	dropStartTime = startTime.ms;
+	dropEndTime = dropStartTime + dropTotalTime;
 
 	while (dropEndTime < endTime.ms) {
-		for (int i = 0; i < rainCount; i++) {
-			Sprite* sprite = Storyboard::CreateSprite(getPath(Path::Circle), Vector2(leftOfScreen + i * rainSpacing, Vector2::ScreenSize.y / 2));
-			sprite->Move(dropStartTime, dropEndTime, sprite->position, Vector2(sprite->position.x, -Vector2::ScreenSize.y / 2));
-		}
-
-		dropTotalTime = VelocityController(dropTotalTime, acceleration);
-
-		dropStartTime += dropTotalTime;
-		dropEndTime = dropStartTime + dropTotalTime;
+		DrawRain();
 	}
+}
+
+//Generates rain sprites and moves them
+void RainGenerator::DrawRain() {
+	for (int i = 0; i < rainCount; i++) {
+		Sprite* sprite = Storyboard::CreateSprite(getPath(Path::Circle), Vector2(leftOfScreen + i * rainSpacing, Vector2::ScreenSize.y / 2));
+		sprite->Move(dropStartTime, dropEndTime, sprite->position, Vector2(sprite->position.x, -Vector2::ScreenSize.y / 2));
+	}
+
+	VelocityController();
+
+	//Increments time after row of rain is finished falling
+	dropStartTime += dropTotalTime;
+	dropEndTime = dropStartTime + dropTotalTime;
 }
 
 //Modifies the velocity of raindrops by changing time a raindrop takes to fall
-float RainGenerator::VelocityController(float dropTotalTime, float acceleration) {
+void RainGenerator::VelocityController() {
 	if (dropTotalTime >= minDropTime) { //Ensures rain doesn't fall faster than minDropTime
-		return dropTotalTime * (2.0f - acceleration);
+		dropTotalTime *= (2.0f - acceleration);
 	}
-	return dropTotalTime;
 }
-
--- Drain/Drain/RainGenerator.hpp
+++ Drain/Drain/RainGenerator.hpp
@@ -5,10 +5,29 @@
 
 class RainGenerator {
 public:
-	RainGenerator(int rainCount, int dropCount, Time startTime, Time endTime, float acceleration = 1.0f);
-	float VelocityController(float dropTotalTime, float acceleration);
+	//Parameters shouldn't be used for RainGenerator, use RainGenerator(); to call function.
+	RainGenerator(int rainCount = 5,
+				  int dropCount = 10,
+				  Time startTime = Time("00:14:00").ms,
+				  Time endTime = Time("00:54:00").ms,
+				  float acceleration = 1.2f);
+
+	void VelocityController();
+	void DrawRain();
 	std::vector<Sprite*> sprites;
 
 private:
+	const int rainCount;
+	const int dropCount;
+	const Time startTime;
+	const Time endTime;
+	const float acceleration;
+
 	float minDropTime = 10.0f;
+	const float leftOfScreen;
+	float dropTotalTime;
+	int totalTime;
+	float rainSpacing;
+	int dropStartTime;
+	int dropEndTime;
 };
