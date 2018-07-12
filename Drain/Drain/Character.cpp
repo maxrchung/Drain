f37c608d9ccf73c43214234f7fdfe28a163bda2c
maxrchung <maxrchung@gmail.com>
2018-07-11 19:47:36 -0700

    Separated Lyric render() into its own function, think this is the least intrusive for Drain.cpp and no need for overlapping; increased stroke thickness; fixed y character issue

--- Drain/Drain/Character.cpp
+++ Drain/Drain/Character.cpp
@@ -249,7 +249,6 @@ std::vector<std::unique_ptr<Stroke>> Character::createStrokes(const char charact
 			strokes.push_back(std::make_unique<LineStroke>(Vector2(-0.5f, 0.5f), Vector2(-0.5f, 0.0f)));
 			strokes.push_back(CircularStroke::create(Vector2(-0.5f, 0.0f), Vector2(0.0f, -0.5f), Vector2::Zero));
 			strokes.push_back(CircularStroke::create(Vector2(0.0f, -0.5f), Vector2(0.5f, 0.0f), Vector2::Zero));
-			strokes.push_back(std::make_unique<LineStroke>(Vector2(0.5f, 0.5f), Vector2(0.5f, 0.0f)));
 			strokes.push_back(std::make_unique<LineStroke>(Vector2(0.5f, 0.0f), Vector2(0.5f, 0.5f)));
 			strokes.push_back(std::make_unique<LineStroke>(Vector2(0.5f, 0.5f), Vector2(0.5f, -0.75f)));
 			strokes.push_back(CircularStroke::create(Vector2(0.5f, -0.75f), Vector2(0.0f, -1.25f), Vector2(0.0f, -0.75f)));
--- Drain/Drain/Lyric.hpp
+++ Drain/Drain/Lyric.hpp
@@ -10,12 +10,13 @@
 // I decided to use a static method over constructor because this doesn't need to return an object
 class Lyric {
 public:
+	static void render();
+private:
 	static void draw(const std::string& lyric,
 					 const Vector2& center,
 					 const int startTime,
 					 const int endTime,
 					 const float scale);
-private:
 	// Space between characters represented as a factor of scale
 	static constexpr float tracking = 0.18f;
 	static float calculateWidth(const std::vector<Character>& characters, const float scale);
