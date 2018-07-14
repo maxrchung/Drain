3b9f8fba9df08d4da6edf37ed64cdf05d1a0a5ed
Aaron Liao <aliao4@uci.edu>
2018-07-13 21:07:28 -0700

    changed function name, default resolution

--- Drain/Drain/Sketch.cpp
+++ Drain/Drain/Sketch.cpp
@@ -45,7 +45,7 @@ void Sketch::draw(Bezier b) {
 	}
 }
 
-const int Sketch::parse() {
+const int Sketch::make() {
     // returns 0 upon success
     std::size_t pos = pointMapPath.find(".txt");
     if (pos == std::string::npos)
@@ -88,6 +88,7 @@ const int Sketch::parse() {
 }
 
 void Sketch::getTransform(float *xshift, float *yshift, float *xscale, float *yscale) {
+    // This function transforms the entire image to be centered and scaled to fit
     std::ifstream ifs;
     ifs.open(pointMapPath);
     auto xvalues = std::vector<float>();
