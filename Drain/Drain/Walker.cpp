e6759d5171b0ad10d7216c30a9d366c11487b0de
Royce <rrysato@gmail.com>
2018-07-15 12:40:55 -0700

    Walker.cpp/hpp: switched the hpp and cpp on accident wth?

--- Drain/Drain/Walker.cpp
+++ Drain/Drain/Walker.cpp
@@ -1,16 +1,42 @@
-#include "Walker.hpp"
+#include "Vector3.hpp"
 
-#include "Time.hpp"
-#include "Vector2.hpp"
+#include <cmath>
 
-class Walker {
-public:
-	float fov = 90;
+Walker::Walker(std::vector<Vector3> sprites, std:;vector<Vector2> location)
+	:sprites(sprites) {
+	for(int i = 0; i < location.size(); ++i) {
+		this->location[i] = convertThreeD(location[i]);
+	}
+}
+
+~Walker::Walker() {
+}
+
+void Walker::walk(Vector2 startPosition, Vector2 endPosition, Time startTime, tiome endTime) {
+	return;
+}
+
+/*
+ * angle vectors are for the azimuth and altitude respectively
+ * these are absolute positioning in degrees
+ */
+void Walker::rotate(Vector2 startAngle, Vector2 endAngle, Time startTime, Time endTime) {
+	//check that the altitude angles are within range
+	if((fabs(startAngle.y) > 90) || (fabs(endAngle.y) > 90))
+		return;
+
+	//modulo the angle so we don't have to deal with that later
+	startAngle.x = fmod(startAngle.x, 360);
+	endAngle.x = fmod(startAngle.x, 360);
+
+
+
+	return;
+}
 	
-	Walker();
-	~Walker();
 
-	void walk(Vector2 startPosition, Vector2 endPosition, Time startTime, Time endTime);
-	void rotate(Vector2 startAngle, Vector2 endAngle, Time startTime, Time endTime);
-private:
+Vector3 Walker::converThreeD(Vector2 coordinates, float size) {
+	Vector3 out;
+	
+	return out;
 }
--- Drain/Drain/Walker.hpp
+++ Drain/Drain/Walker.hpp
@@ -1,34 +1,26 @@
 #pragma once
 
-#include "Vector3.hpp"
+#include "Walker.hpp"
 
-#include <cmath>
+#include "Time.hpp"
+#include "Vector2.hpp"
 
-Walker::Walker() {
-}
-
-~Walker::Walker() {
-}
-
-void Walker::walk(Vector2 startPosition, Vector2 endPosition, Time startTime, tiome endTime) {
-	return;
-}
-
-/*
- * angle vectors are for the azimuth and altitude respectively
- * these are absolute positioning in degrees
- */
-void Walker::rotate(Vector2 startAngle, Vector2 endAngle, Time startTime, Time endTime) {
-	//check that the altitude angles are within range
-	if((fabs(startAngle.y) > 90) || (fabs(endAngle.y) > 90))
-		return;
-
-	//modulo the angle so we don't have to deal with that later
-	startAngle.x = fmod(startAngle.x, 360);
-	endAngle.x = fmod(startAngle.x, 360);
-
-
-
-	return;
-}
+class Walker {
+public:
+	//field of view
+	float fov = 90;
 	
+	//vector of sprites currently on screen
+	std::vector<Sprite *> sprites;
+
+	//location of each sprite
+	std::vector<Vector3> location;
+
+	Walker(std::vector<Sprite *> sprites, std::vector<Vector2> location);
+	~Walker();
+
+	void walk(Vector2 startPosition, Vector2 endPosition, Time startTime, Time endTime);
+	void rotate(Vector2 startAngle, Vector2 endAngle, Time startTime, Time endTime);
+private:
+	Vector3 convertThreeD(Vector2);
+}
