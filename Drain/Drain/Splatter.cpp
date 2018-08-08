0d3b18856906da9512dc7c31ddcc1f97e6f11c74
Aaron Liao <aliao4@uci.edu>
2018-08-07 22:46:09 -0700

    added more tweakability to drawing splatter drops

--- Drain/Drain/Splatter.cpp
+++ Drain/Drain/Splatter.cpp
@@ -4,33 +4,41 @@
 
 // standalone, defined by pos Vector2
 Splatter::Splatter(const Time& startTime, const Time& endTime,
-    const float size, const int numDrops, const Easing& easing, const Vector2 pos)
+    const float size, const int numDrops, const Vector2 pos)
     : startTime{ startTime }, endTime{ endTime },
-    size{ size }, numDrops{ numDrops }, easing{ easing }, pos { pos }
+    sizeFactor{ size }, numDrops{ numDrops }, pos { pos }
 {
+    visDur = endTime.ms - startTime.ms;
 }
 
 // position defined by existing AirBubble
 Splatter::Splatter(const Time& startTime, const Time& endTime,
-    const float size, const int numDrops, const Easing& easing, const Sketch * bubble)   // replace with AirBubble
-    : startTime{ startTime }, endTime{ endTime },
-    size{ size }, numDrops{ numDrops }, easing{ easing }, bubble{ bubble }
+    const float size, const int numDrops, const Sketch * bubble)   // replace with AirBubble
+    : Splatter(startTime, endTime, size, numDrops, Vector2(0,0))   // replace 0,0 with bubble->loc
 {
 }
 
 // returns and draws a single circle at pos after offset ms
-Sprite * Splatter::draw(Vector2 pos, int offset) {
+Sprite * Splatter::draw(Vector2 pos, int offset, float size,
+    float fadeInDur, float fadeFrom, float growDur, float growFromSize, Easing easing)
+{
     auto s = Storyboard::CreateSprite(getPath(Path::Circle), pos);
-    s->Scale(startTime.ms + offset, endTime.ms, size, size);
-    Swatch::colorFgToFgSprites({ s }, startTime.ms, startTime.ms);
+    if (growDur != 0)
+        s->Scale(startTime.ms + offset, startTime.ms + offset + growDur, size * growFromSize, size, easing);
+    else
+        s->Scale(startTime.ms + offset, startTime.ms + offset, size, size); // scale to proper size instantaneously
+    if (fadeInDur != 0)
+        s->Fade(startTime.ms + offset, startTime.ms + offset + fadeInDur, fadeFrom, 1, easing);
+    Swatch::colorFgToFgSprites({ s }, startTime.ms, endTime.ms); // color will determine Sprite lifetime
     return s;
 }
 
 // returns number of sprites drawn, going for something like this: http://gifimage.net/splatter-gif-10/
 int Splatter::make() {
-    auto centerPoint = draw(pos, 0);
+    // main droplet will grow from 80% to 100% of its size over 3/4 of its lifespan
+    auto centerPoint = draw(pos, 0, sizeFactor, 0, 0, (endTime.ms - startTime.ms) * 0.75, 0.8, Easing::EasingOut);
     int ans = 1;
-    // todo: add bleeding from centerPoint
+    // todo: add satellite drops to emulate bleeding from centerPoint
     for (int i = 1; i < numDrops; i++) {
         // todo: easing for offset
         // do my magic here
@@ -42,5 +50,5 @@ int Splatter::make() {
 
 void Splatter::render() {
     std::cout << "Rendering Splatter..." << std::endl;
-    Splatter(Time("00:00:900"), Time("00:04:200"), 0.5, 20, Easing::Linear, Vector2(50, 50)).make();
+    Splatter(Time("00:00:900"), Time("00:04:200"), 0.5, 20, Vector2(50, 50)).make();
 }
--- Drain/Drain/Splatter.hpp
+++ Drain/Drain/Splatter.hpp
@@ -12,25 +12,30 @@ public:
         const Time& endTime,
         const float size,
         const int numDrops,
-        const Easing& easing,
         const Vector2 pos);
     // position defined by existing AirBubble
     Splatter(const Time& startTime,
-             const Time& endTime,
-             const float size,
-             const int numDrops,
-             const Easing& easing,
-             const Sketch * bubble);   // Sketch is a placeholder, replace with AirBubble
+        const Time& endTime,
+        const float size,
+        const int numDrops,
+        const Sketch * bubble);   // Sketch is a placeholder, replace with AirBubble
     static void render();
 private:
     const Time startTime;
     const Time endTime;
+    int visDur;
     const Vector2 pos;
-    const float size;
+    const float sizeFactor;
     const int numDrops;
-    const Easing easing;
     const Sketch * bubble;   // replace with AirBubble
-    Sprite * draw(Vector2 pos, int offset);
+    Sprite * draw(Vector2 pos,
+        int offset,
+        float size,
+        float fadeInDur = 0,    // duration of fade in ms
+        float fadeFrom = 0,     // origin opacity from 0 to 1
+        float growDur = 0,      // duration of expansion in ms
+        float growFrom = 0,     // origin size from 0 to 1
+        Easing easing = Easing::Linear);
     int make();
 };
 
